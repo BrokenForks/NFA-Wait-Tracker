c91bdcf569eab4ff1feb4a9691895d465478430c
BrokenForks <136125840+BrokenForks@users.noreply.github.com>
2023-06-09 23:44:58 -0400

    Update and rename NFATracker Startscript.cmd to NFATracker Startscript.cmd
    Updated for clarity.

--- NFATracker Startscript.cmd
+++ NFATracker Startscript.cmd
@@ -1,10 +1,14 @@
-: Quick and dirty script to compile and launch the NFA Wait Tracker
-: It probably doesn't work on your system, but if it does, more power to you.
-: To even have a chance you need to download and install Java Development Kit (JDK)
-: And make sure javac (The compiler) is added to your PATH system variable.
+:: Quick and dirty Windows Command Processor script to compile and launch the NFA Wait Tracker
+:: It probably doesn't work on your system, but if it does, more power to you.
+:: To even have a chance you need to download and install Java Development Kit (JDK)
+:: And make sure javac (The compiler) is added to your PATH system variable.
+:: This file also needs to be in the same directory as NFAWait.java to work properly.
 
+::Disable console echo
 @echo off
 
+::If the compiled version of NFA Wait Tracker does not exist, run the compiler to create it.
 IF NOT EXIST NFAWait.class (javac NFAWait.java)
 
+::Run the compiled version of NFA Wait Tracker. 
 java NFAWait
